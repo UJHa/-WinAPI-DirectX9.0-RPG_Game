b8e9dea9191257b32b8f92c9d9785a712ce40ba3
testmodule <ad112468@naver.com>
2017-09-29 19:51:31 +0900

    directX 11>9 change, fullScreen Setting

--- WinGameRPG/WinGameRPG/GameSystem.cpp
+++ WinGameRPG/WinGameRPG/GameSystem.cpp
@@ -21,11 +21,14 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lparam) {
 GameSystem* GameSystem::_instance = NULL;
 GameSystem::GameSystem()
 {
-	_isEnable4xMsaa = false;
+	//_isEnable4xMsaa = false;
+	_isFullScreen = false;
 }
 GameSystem::~GameSystem()
 {
-	_swapChain->Release();
+	RELEASE_COM(_sprite);
+	RELEASE_COM(_device3d);
+	/*_swapChain->Release();
 	_swapChain = 0;
 
 	_d3dDevice->Release();
@@ -41,7 +44,7 @@ GameSystem::~GameSystem()
 	_depthStencilBuffer = 0;
 
 	_depthStencilView->Release();
-	_depthStencilView = 0;
+	_depthStencilView = 0;*/
 }
 GameSystem* GameSystem::GetInstance()
 {
@@ -68,7 +71,39 @@ bool GameSystem::InitSystem(HINSTANCE hInstance, int nCmdShow)
 		return false;
 	}
 	//윈도우 생성
-	_hMainWnd = CreateWindow(L"Base", L"2D_RPG_TileMap", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, hInstance, 0);
+	DWORD style;
+	if (_isFullScreen)
+	{
+		style = WS_EX_TOPMOST | WS_VISIBLE | WS_POPUP;
+	}
+	else
+	{
+		style = WS_OVERLAPPEDWINDOW;
+	}
+	_hMainWnd = CreateWindow(
+		L"Base",
+		L"2D_RPG_TileMap",
+		style,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		0,
+		0,
+		hInstance,
+		0);
+
+	if (false == _isFullScreen)
+	{
+		RECT clientRect;
+		GetClientRect(_hMainWnd, &clientRect);
+
+		int addWidth = 1280 - clientRect.right;
+		int addHeight = 800 - clientRect.bottom;
+		int finalWidth = 1280 + addWidth;
+		int finalHeight = 800 + addHeight;
+		MoveWindow(_hMainWnd, 0, 0, finalWidth, finalHeight, TRUE);
+	}
 	//윈도우 출력
 	ShowWindow(_hMainWnd, nCmdShow);
 	//윈도우 업데이트
@@ -109,19 +144,28 @@ int GameSystem::Update()
 
 				frameDuration = 0.0f;
 
-				colorRed += 0.1f;
-				if(colorRed>1.0f)
-					colorRed -= 1.0f;
-				float color[4];
-				color[0] = colorRed;	//R
-				color[1] = 0.2f;	//G
-				color[2] = 0.3f;	//B
-				color[3] = 1.0f;	//Alpha
+				//colorRed += 0.1f;
+				//if(colorRed>1.0f)
+				//	colorRed -= 1.0f;
+				//float color[4];
+				//color[0] = colorRed;	//R
+				//color[1] = 0.2f;	//G
+				//color[2] = 0.3f;	//B
+				//color[3] = 1.0f;	//Alpha
 
-				_d3dDeviceContext->ClearRenderTargetView(_renderTargetView, color);
-				_d3dDeviceContext->ClearDepthStencilView(_depthStencilView, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+				//_d3dDeviceContext->ClearRenderTargetView(_renderTargetView, color);
+				//_d3dDeviceContext->ClearDepthStencilView(_depthStencilView, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
 
-				_swapChain->Present(0, 0);
+				//_swapChain->Present(0, 0);
+				_device3d->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 100, 0), 0.0f, 0);
+				_device3d->BeginScene();
+
+				_sprite->Begin(0);
+
+				_sprite->End();
+
+				_device3d->EndScene();
+				_device3d->Present(NULL, NULL, NULL, NULL);
 			}
 
 			/*wchar_t timeCheck[256];
@@ -135,13 +179,14 @@ int GameSystem::Update()
 }
 bool GameSystem::InitDirect3D()
 {
+	/*
 	D3D_FEATURE_LEVEL featureLevel;
 	HRESULT hr = D3D11CreateDevice(0,D3D_DRIVER_TYPE_HARDWARE,0,D3D10_CREATE_DEVICE_DEBUG,0,0,D3D11_SDK_VERSION,&_d3dDevice,&featureLevel,&_d3dDeviceContext);
 	hr = _d3dDevice->CheckMultisampleQualityLevels(DXGI_FORMAT_R8G8B8A8_UNORM, 4, &_4xMsaaQuality);	//4xMSAA 지원여부 확인
 	if (FAILED(hr))
 	{
-		MessageBox(0, L"4xMSAA 에러입니다.", L"ErrorMessage", 0);
-		return false;
+	MessageBox(0, L"4xMSAA 에러입니다.", L"ErrorMessage", 0);
+	return false;
 	}
 	//클라이언트 화면 크기의 특징 서술
 	DXGI_SWAP_CHAIN_DESC swapChain;
@@ -154,13 +199,13 @@ bool GameSystem::InitDirect3D()
 	swapChain.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
 	if (_isEnable4xMsaa)
 	{
-		swapChain.SampleDesc.Count = 4;
-		swapChain.SampleDesc.Quality = _4xMsaaQuality-1;
+	swapChain.SampleDesc.Count = 4;
+	swapChain.SampleDesc.Quality = _4xMsaaQuality-1;
 	}
 	else
 	{
-		swapChain.SampleDesc.Count = 1;
-		swapChain.SampleDesc.Quality = 0;
+	swapChain.SampleDesc.Count = 1;
+	swapChain.SampleDesc.Quality = 0;
 	}
 	swapChain.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
 	swapChain.BufferCount = 1;
@@ -174,29 +219,29 @@ bool GameSystem::InitDirect3D()
 	hr = _d3dDevice->QueryInterface(__uuidof(IDXGIDevice), (void**)&dxgiDevice);
 	if (FAILED(hr))
 	{
-		MessageBox(0, L"dxgiDevice 에러입니다.", L"ErrorMessage", 0);
-		return false;
+	MessageBox(0, L"dxgiDevice 에러입니다.", L"ErrorMessage", 0);
+	return false;
 	}
 	IDXGIAdapter* dxgiAdapter = 0;
 	hr = dxgiDevice->GetParent(__uuidof(IDXGIAdapter), (void**)&dxgiAdapter);
 	if (FAILED(hr))
 	{
-		MessageBox(0, L"dxgiAdapter 에러입니다.", L"ErrorMessage", 0);
-		return false;
+	MessageBox(0, L"dxgiAdapter 에러입니다.", L"ErrorMessage", 0);
+	return false;
 	}
 	IDXGIFactory* dxgiFactory = 0;
 	hr = dxgiAdapter->GetParent(__uuidof(IDXGIFactory), (void**)&dxgiFactory);
 	if (FAILED(hr))
 	{
-		MessageBox(0, L"dxgiFactory 에러입니다.", L"ErrorMessage", 0);
-		return false;
+	MessageBox(0, L"dxgiFactory 에러입니다.", L"ErrorMessage", 0);
+	return false;
 	}
 
 	hr = dxgiFactory->CreateSwapChain(_d3dDevice,&swapChain,&_swapChain);
 	if (FAILED(hr))
 	{
-		MessageBox(0, L"dxgiFactory 에러입니다.", L"ErrorMessage", 0);
-		return false;
+	MessageBox(0, L"dxgiFactory 에러입니다.", L"ErrorMessage", 0);
+	return false;
 	}
 	//메모리 해제
 	dxgiDevice->Release();
@@ -211,19 +256,19 @@ bool GameSystem::InitDirect3D()
 	//SwapChain 후면 버퍼 대한 랜더 대상 뷰 생성
 	ID3D11Texture2D* backBuffer;
 	hr = _swapChain->GetBuffer(
-		0,	//백버퍼 인덱스
-		__uuidof(ID3D11Texture2D),
-		reinterpret_cast<void**>(&backBuffer));
+	0,	//백버퍼 인덱스
+	__uuidof(ID3D11Texture2D),
+	reinterpret_cast<void**>(&backBuffer));
 	if (FAILED(hr))
 	{
-		MessageBox(0, L"backBuffer 에러입니다.", L"ErrorMessage", 0);
-		return false;
+	MessageBox(0, L"backBuffer 에러입니다.", L"ErrorMessage", 0);
+	return false;
 	}
 	hr = _d3dDevice->CreateRenderTargetView(backBuffer, 0, &_renderTargetView);
 	if (FAILED(hr))
 	{
-		MessageBox(0, L"_renderTargetView 에러입니다.", L"ErrorMessage", 0);
-		return false;
+	MessageBox(0, L"_renderTargetView 에러입니다.", L"ErrorMessage", 0);
+	return false;
 	}
 	backBuffer->Release();
 	backBuffer = 0;
@@ -237,13 +282,13 @@ bool GameSystem::InitDirect3D()
 
 	if (_isEnable4xMsaa)
 	{
-		depthStencilDesc.SampleDesc.Count = 4;
-		depthStencilDesc.SampleDesc.Quality = _4xMsaaQuality;
+	depthStencilDesc.SampleDesc.Count = 4;
+	depthStencilDesc.SampleDesc.Quality = _4xMsaaQuality;
 	}
 	else
 	{
-		depthStencilDesc.SampleDesc.Count = 1;
-		depthStencilDesc.SampleDesc.Quality = 0;
+	depthStencilDesc.SampleDesc.Count = 1;
+	depthStencilDesc.SampleDesc.Quality = 0;
 	}
 	depthStencilDesc.Usage = D3D11_USAGE_DEFAULT;
 	depthStencilDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
@@ -252,15 +297,15 @@ bool GameSystem::InitDirect3D()
 	hr = _d3dDevice->CreateTexture2D(&depthStencilDesc, 0, &_depthStencilBuffer);
 	if (FAILED(hr))
 	{
-		MessageBox(0, L"_depthStencilBuffer 에러입니다.", L"ErrorMessage", 0);
-		return false;
+	MessageBox(0, L"_depthStencilBuffer 에러입니다.", L"ErrorMessage", 0);
+	return false;
 	}
 
 	hr = _d3dDevice->CreateDepthStencilView(_depthStencilBuffer, 0, &_depthStencilView);
 	if (FAILED(hr))
 	{
-		MessageBox(0, L"_depthStencilView 에러입니다.", L"ErrorMessage", 0);
-		return false;
+	MessageBox(0, L"_depthStencilView 에러입니다.", L"ErrorMessage", 0);
+	return false;
 	}
 	// 렌더 대상 뷰와 깊이 스텐실 뷰를 Direct3D가 사용할 수 있도록 렌더링 파이프리인의 출력 병합기에 묶는다.
 	_d3dDeviceContext->OMSetRenderTargets(1, &_renderTargetView, _depthStencilView);
@@ -273,6 +318,66 @@ bool GameSystem::InitDirect3D()
 	_screenViewport.MinDepth = 0.0f;
 	_screenViewport.MaxDepth = 1.0f;
 
-	_d3dDeviceContext->RSSetViewports(1, &_screenViewport);
+	_d3dDeviceContext->RSSetViewports(1, &_screenViewport);*/
+	LPDIRECT3D9 direct3d = Direct3DCreate9(D3D_SDK_VERSION);
+
+	if (NULL == direct3d)
+	{
+		MessageBox(0, L"false", L"create Filed", MB_OK);
+		return false;
+	}
+
+	D3DPRESENT_PARAMETERS d3dpp;
+	ZeroMemory(&d3dpp, sizeof(d3dpp));
+	d3dpp.BackBufferWidth = 1280;
+	d3dpp.BackBufferHeight = 800;
+
+	if (_isFullScreen)
+	{
+		d3dpp.BackBufferFormat = D3DFMT_X8R8G8B8;
+	}
+	else
+	{
+		d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+	}
+
+	d3dpp.BackBufferCount = 1;
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	d3dpp.hDeviceWindow = _hMainWnd;
+	d3dpp.Windowed = (!_isFullScreen);
+	d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_IMMEDIATE;
+
+	D3DCAPS9 caps;
+	HRESULT hr = direct3d->GetDeviceCaps(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, &caps);
+	if (FAILED(hr))
+	{
+		MessageBox(0, L"GetDeviceCaps 에러입니다.", L"ErrorMessage", 0);
+		return false;
+	}
+
+	DWORD behavior;
+	if ((caps.DeviceType & D3DDEVCAPS_HWTRANSFORMANDLIGHT) == 0 || (caps.VertexShaderVersion < D3DVS_VERSION(1, 1)))
+	{
+		behavior = D3DCREATE_SOFTWARE_VERTEXPROCESSING;
+	}
+	else
+	{
+		behavior = D3DCREATE_HARDWARE_VERTEXPROCESSING;
+	}
+
+	hr = direct3d->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, _hMainWnd, behavior, &d3dpp, &_device3d);
+	if (FAILED(hr))
+	{
+		MessageBox(0, L"CreateDevice 에러입니다.", L"ErrorMessage", 0);
+		return false;
+	}
+
+	hr = D3DXCreateSprite(_device3d, &_sprite);
+	if (FAILED(hr))
+	{
+		MessageBox(0, L"D3DXCreateSprite 에러입니다.", L"ErrorMessage", 0);
+		return false;
+	}
+
 	return true;
 }
--- WinGameRPG/WinGameRPG/GameSystem.h
+++ WinGameRPG/WinGameRPG/GameSystem.h
@@ -1,6 +1,10 @@
 #pragma once
 #include <Windows.h>
-#include<D3DX11.h>
+//#include<D3DX11.h>
+#include<D3DX9.h>
+#include<D3D9.h>
+#define RELEASE_COM(x) {if(x){x->Release();x=NULL;}}
+#define SAVE_DELETE(x) {if(x){delete x;x=NULL;}}
 struct GameTimer;
 class GameSystem
 {
@@ -15,17 +19,20 @@ public:
 	int Update();
 	bool InitDirect3D();
 private:
-	ID3D11Device* _d3dDevice;
-	ID3D11DeviceContext* _d3dDeviceContext;
 	UINT _4xMsaaQuality;
-	bool _isEnable4xMsaa;
 	HWND _hMainWnd;
-	IDXGISwapChain* _swapChain;
-	ID3D11RenderTargetView* _renderTargetView;
-	ID3D11Texture2D* _depthStencilBuffer;
-	ID3D11DepthStencilView* _depthStencilView;
+	//ID3D11Device* _d3dDevice;
+	//ID3D11DeviceContext* _d3dDeviceContext;
+	//bool _isEnable4xMsaa;
+	//IDXGISwapChain* _swapChain;
+	//ID3D11RenderTargetView* _renderTargetView;
+	//ID3D11Texture2D* _depthStencilBuffer;
+	//ID3D11DepthStencilView* _depthStencilView;
 
-	D3D11_VIEWPORT _screenViewport;
+	//D3D11_VIEWPORT _screenViewport;
+	LPDIRECT3DDEVICE9 _device3d;
+	LPD3DXSPRITE _sprite;
+	bool _isFullScreen;
 private:
 	GameTimer* _gameTimer;
 };
